57355a104ca5cc7226eab481c5e6132ed2dcf92b
Madrobot9182 <65980347+Madrobot9182@users.noreply.github.com>
2025-02-23 12:27:13 -0700

    Renamed script folder

---
+++ Assets/Scripts/NPCDialog/NPCDialogueHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCDialogueHandler : MonoBehaviour {
+    public List<string> dialogueLines;
+    private int currentLineIndex = 0; 
+    [HideInInspector] public Action afterDialogue;
+
+    public string GetNextLine() {
+        if (currentLineIndex < dialogueLines.Count) {
+            return dialogueLines[currentLineIndex++];
+        } else {
+            return null;  // No more lines
+        }
+    }
+
+    public void ResetDialogue() {
+        currentLineIndex = 0;
+    }
+
+    void Awake() {
+    }
+    
+    void Start() {
+    }
+
+    void Update() {
+    }
+}
