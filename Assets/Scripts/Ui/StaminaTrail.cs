9ec08fadc2cc5da36e5d412009dab7b8c3a536a4
Madrobot9182 <65980347+Madrobot9182@users.noreply.github.com>
2025-02-23 14:28:12 -0700

    Lint check fix

--- Assets/Scripts/Ui/StaminaTrail.cs
+++ Assets/Scripts/Ui/StaminaTrail.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StaminaTrail : MonoBehaviour
-{
+public class StaminaTrail : MonoBehaviour {
     public Image barRef, trailRef;
     public float trailSize = 5f;
 
@@ -12,21 +11,21 @@ public class StaminaTrail : MonoBehaviour
     private Color sStartColor = new Color(0.423f, 1f, 0.004f), sEndColor = new Color(1f, 0f, 0f);
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         trailRef = GetComponent<Image>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (trailRef.fillAmount >= barRef.fillAmount){
+    void Update() {
+        if (trailRef.fillAmount >= barRef.fillAmount) {
             trailRef.fillAmount = Mathf.Lerp(trailRef.fillAmount, barRef.fillAmount, Time.deltaTime * trailSize);
-        } else {trailRef.fillAmount = barRef.fillAmount;}
+        } else {
+            trailRef.fillAmount = barRef.fillAmount;
+        }
 
         // Gradually Shifts the Color based on Image Lenght
         float gradBar = Mathf.InverseLerp(.4f, 0f, barRef.fillAmount);
-        
+
         barRef.color = Color.Lerp(sStartColor, sEndColor, gradBar);
         trailRef.color = Color.Lerp(bStartColor, bEndColor, gradBar);
     }
