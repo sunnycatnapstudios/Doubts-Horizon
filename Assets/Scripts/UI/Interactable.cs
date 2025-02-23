7503f98e3b8c93772e3ecd766c54f05593b4f948
Chetan Tyagi <87710619+chetanty@users.noreply.github.com>
2025-02-23 08:37:43 +0530

    Update Interactable.cs

--- Assets/Scripts/UI/Interactable.cs
+++ Assets/Scripts/UI/Interactable.cs
@@ -1,5 +1,10 @@
-using UnityEngine;
 using TMPro;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class Interactable : MonoBehaviour
 {
