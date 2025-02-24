e66d027253c5f145ac9aaadd09bde758a8bad357
Madrobot9182 <65980347+Madrobot9182@users.noreply.github.com>
2025-02-23 20:08:50 -0700

    Lint check fix

--- Assets/Scripts/Audio/CustomTileTypes.cs
+++ Assets/Scripts/Audio/CustomTileTypes.cs
@@ -6,5 +6,10 @@ using UnityEngine.Tilemaps;
 public class CustomTileTypes : TileBase {
     public TileType tileType; // Enum for tile types
 
-    public enum TileType { Dirt, Stone, Water, Wood}
+    public enum TileType {
+        Dirt,
+        Stone,
+        Water,
+        Wood
+    }
 }
