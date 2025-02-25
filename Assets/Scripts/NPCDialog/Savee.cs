f2dba8a0628ee85030e3196b4e80eb3d3edbe1d2
Kaaden RumanCam <kaaden@ualberta.ca>
2025-02-25 05:08:34 -0700

    forgot to change class name

--- Assets/Scripts/NPCDialog/Savee.cs
+++ Assets/Scripts/NPCDialog/Savee.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JoinerDialogue : MonoBehaviour {
+public class SaveeDialogue : MonoBehaviour {
     private DialogueInputHandler dialogueInputHandler;
     private NPCDialogueHandler npcDialogueHandler;
     public Survivor survivor;
