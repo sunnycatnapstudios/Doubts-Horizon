b2fe10875b4120350dddc54d715ec50f5c7f92f0
emasudaualberta <emasuda@ualberta.ca>
2025-02-25 04:57:33 -0700

    loads and deloads party members in campfire zone

--- Assets/fireplace.cs
+++ Assets/fireplace.cs
@@ -5,28 +5,43 @@ using UnityEngine;
 public class fireplace : MonoBehaviour
 {
 
-    public List<GameObject> positions;
+    public List<GameObject> objects;
     public Vector3 positionSpawn;
     private Vector3 originalLoc;
     private bool isActive;
     private GameObject player;
+    private PartyManager partyManager;
 
 
     public Dictionary<string, GameObject> possibleNpcs;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
-
-        
+        partyManager = player.GetComponent<PartyManager>();
     }
 
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D other) {
         
+        SpawnMembers();
+        
     }
 
     private void SpawnMembers() {
+        foreach (GameObject npc in objects) {
+            Identity identity = npc.GetComponent<Identity>(); ;
+            if (identity != null) {
+                Debug.Log(identity.GetSurvivor().GetName());
+
+                Survivor sur = identity.GetSurvivor();
+                if (!partyManager.currentPartyMembers.Contains(sur)) {
+                    npc.SetActive(false);
+
+                } else {
+                    npc.SetActive(true);
+                }
+            }
+        }
 
     }
 }
