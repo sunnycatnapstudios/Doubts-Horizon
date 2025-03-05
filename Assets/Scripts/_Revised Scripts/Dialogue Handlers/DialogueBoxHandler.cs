8acd899f1cc3bff0662739c222730277505f22a1
Kaaden RumanCam <kaaden@ualberta.ca>
2025-03-05 16:00:33 -0700

    format DialogueBoxHandler

--- Assets/Scripts/_Revised Scripts/Dialogue Handlers/DialogueBoxHandler.cs
+++ Assets/Scripts/_Revised Scripts/Dialogue Handlers/DialogueBoxHandler.cs
@@ -3,35 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DialogueBoxHandler : MonoBehaviour
-{
+public class DialogueBoxHandler : MonoBehaviour {
     public List<string> dialogueContents = new List<string>();
     public int currentLineIndex = 0;
     public Sprite npcProfile;
     public bool lastLineDisplayed = false, hasChoice;
 
-    void Start()
-    {
-        if (this.CompareTag("NPC"))
-        {
+    void Start() {
+        if (this.CompareTag("NPC")) {
 
-        }
-        else if (this.CompareTag("Interactable"))
-        {
-            
+        } else if (this.CompareTag("Interactable")) {
+
         }
     }
 
-    public string GetCurrentDialogueLine()
-    {
-        if (dialogueContents.Count == 0) return "";
-        
+    public string GetCurrentDialogueLine() {
+        if (dialogueContents.Count == 0) {
+            return "";
+        }
+
         string currentLine = dialogueContents[currentLineIndex];
 
-        if (currentLineIndex < dialogueContents.Count - 1) {currentLineIndex++;}
-        else {lastLineDisplayed = true;}
+        if (currentLineIndex < dialogueContents.Count - 1) {
+            currentLineIndex++;
+        } else {
+            lastLineDisplayed = true;
+        }
 
         return currentLine;
     }
-    public bool CanClose() {return lastLineDisplayed;}
+
+    public bool CanClose() {
+        return lastLineDisplayed;
+    }
 }
