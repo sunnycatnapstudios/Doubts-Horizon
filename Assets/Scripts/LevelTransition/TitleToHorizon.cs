32a17ebe622e9e9a1d5557ad19f1ec5d8b243404
Madrobot9182 <65980347+Madrobot9182@users.noreply.github.com>
2025-03-15 21:55:29 -0600

    Added fade to black

---
+++ Assets/Scripts/LevelTransition/TitleToHorizon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleToHorizon : MonoBehaviour {
+    public bool isClicked = false; // Ensure no spam clicking
+    public GameObject blackFader;
+    RawImage blackFaderImage;
+
+    // Trigger on button click
+    public void OnStartButtonClicked() {
+        if (isClicked) {
+            return;
+        } else {
+            isClicked = true;
+        }
+
+        // Fade the canvas to black
+        StartCoroutine(FadeOutToBlack());
+    }
+
+    private IEnumerator FadeOutToBlack() {
+        blackFader.gameObject.SetActive(true);
+        while (blackFaderImage.color.a < 1) {
+            float fadeAmount = blackFaderImage.color.a + (Time.deltaTime * 0.8f);
+            Color newColor = new Color(blackFaderImage.color.r, blackFaderImage.color.g, blackFaderImage.color.b,
+                fadeAmount);
+            blackFaderImage.color = newColor;
+            yield return null;
+        }
+    }
+    public void Awake() {
+        blackFaderImage = blackFader.GetComponent<RawImage>();
+    }
+}
