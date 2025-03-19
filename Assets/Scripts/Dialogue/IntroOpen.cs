e9fcb8bc3e412ab98de4d40de0135b02a1f89a9a
Kaaden RumanCam <kaaden@ualberta.ca>
2025-03-19 00:16:48 -0600

    opening explanation dialogue of intro, broken

---
+++ Assets/Scripts/Dialogue/IntroOpen.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroOpen : MonoBehaviour {
+    private readonly string tagTarget = "Player";
+    private DialogueInputHandler dialogueInputHandler;
+    private DialogueBoxHandler npcDialogueHandler;
+    private Player player;
+
+    private PartyManager manager;
+    public RandomSurvivorCampfireScript afterDialogue;
+    
+   
+    [Serializable]
+    private struct AudioClips {
+        public AudioClip sfxTalkingBlip;
+    }
+
+    [SerializeField] private AudioClips audioClips;
+
+    void Start() {
+        dialogueInputHandler = GameObject.FindGameObjectWithTag("Dialogue Text").GetComponent<DialogueInputHandler>();
+        npcDialogueHandler = GetComponent<DialogueBoxHandler>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        manager = GameObject.FindGameObjectWithTag("Player").GetComponent<PartyManager>();
+
+        npcDialogueHandler.SetSfxTalkingClip(audioClips.sfxTalkingBlip);
+
+        npcDialogueHandler.dialogueContents = new List<string> {
+            "Oh good, you're okay",
+            "*Cough*",
+            "Quick! This place is gonna blow!",
+            "You're gonna have to pull one of us out first.."
+        };
+
+        npcDialogueHandler.afterDialogue = new Action(AfterDialogue);
+    }
+
+    void AfterDialogue() {
+        GameStatsManager.Instance._dialogueHandler.isCloseable = false;
+        GameStatsManager.Instance._dialogueHandler.OpenDialogueWith(afterDialogue.gameObject);
+        GameStatsManager.Instance._dialogueHandler.isCloseable = true;
+    }
+}
