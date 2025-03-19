7363156332c1795cf58169c3297b88ede20f6d37
emasudaualberta <emasuda@ualberta.ca>
2025-03-19 01:57:50 -0600

    added sprites to intro

--- Assets/Scripts/Dialogue/campfireDialogue/CampfireDialogueIntro/IntroCamfireDialogue.cs
+++ Assets/Scripts/Dialogue/campfireDialogue/CampfireDialogueIntro/IntroCamfireDialogue.cs
@@ -1,18 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class IntroCamfireDialogue : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private readonly string tagTarget = "Player";
+    private DialogueInputHandler dialogueInputHandler;
+    private DialogueBoxHandler npcDialogueHandler;
+    private Player player;
+
+    private PartyManager manager;
+    public RandomSurvivorCampfireScript afterDialogue;
+
+
+    [Serializable]
+    private struct AudioClips {
+        public AudioClip sfxTalkingBlip;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [SerializeField] private AudioClips audioClips;
+
+    void Start() {
+        dialogueInputHandler = GameObject.FindGameObjectWithTag("Dialogue Text").GetComponent<DialogueInputHandler>();
+        npcDialogueHandler = GetComponent<DialogueBoxHandler>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        manager = GameObject.FindGameObjectWithTag("Player").GetComponent<PartyManager>();
+
+       
+        npcDialogueHandler.SetSfxTalkingClip(audioClips.sfxTalkingBlip);
+
+        npcDialogueHandler.dialogueContents = new List<string> {
+            "Hey over there!",
+            "That looks like some food!",
+            "Im starving"
+
+        };
+
+        npcDialogueHandler.afterDialogue = new Action(AfterDialogue);
+    }
+
+
+
+    void AfterDialogue() {
+      gameObject.SetActive(false);
     }
 }
