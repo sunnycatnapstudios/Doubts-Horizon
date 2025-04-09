7fa9a0e9e0cfa134ba3e848a0bf60a742875bbf5
chetanty <ctat684@gmail.com>
2025-04-09 17:44:09 -0600

    ending dialogues done and plugged

---
+++ Assets/Scripts/Dialogue/goodendingDialogue/OrphanEndingScript.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrphanEndingScript : MonoBehaviour {
+    private DialogueInputHandler dialogueInputHandler;
+    private DialogueBoxHandler npcDialogueHandler;
+    public Survivor survivor;
+    private GameStatsManager statsManager;
+    public AudioClip sfxTalkingClip;
+
+    void Start() {
+        dialogueInputHandler = GameObject.FindGameObjectWithTag("Dialogue Text").GetComponent<DialogueInputHandler>();
+        npcDialogueHandler = GetComponent<DialogueBoxHandler>();
+        statsManager = GameStatsManager.Instance;
+
+        if (sfxTalkingClip == null) {
+            sfxTalkingClip = survivor.GetTalkingSfx();
+        }
+        npcDialogueHandler.SetSfxTalkingClip(sfxTalkingClip);
+
+        statsManager.interactedWithCampfireNPC();
+        statsManager.updateBedStatus();
+
+        npcDialogueHandler.dialogueContents = new List<string> {
+            "I'm feeling better now... Thank you again for saving me.",
+            "I’ll never forget your kindness."
+        };
+
+        npcDialogueHandler.afterDialogue = AfterDialogue;
+
+        GameStatsManager.Instance._dialogueHandler.UpdateDialogueBox();
+    }
+
+    void AfterDialogue() {
+        Debug.Log("Completed good ending orphan dialogue.");
+    }
+}
+
