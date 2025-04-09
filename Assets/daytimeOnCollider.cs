e00b8cb63f596d01bf0dbe8ec6dba9392d0dfc19
emasudaualberta <emasuda@ualberta.ca>
2025-04-09 17:39:41 -0600

    daytime collider script

---
+++ Assets/daytimeOnCollider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class daytimeOnCollider : MonoBehaviour
+{
+
+    private readonly string tagTarget = "Player";
+    private Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+   void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag(tagTarget)) {
+            if (player == null) {
+
+                GameObject playerObject = other.gameObject;
+                player = other.gameObject.GetComponent<Player>();
+            }
+            GameStatsManager.Instance.nightFilter.SetActive(true);
+        }
+
+
+    }
+}
